227d586460b2dee01ec1c0d28b43cda1ad4085b8
bkonkle <brandon.konkle@gmail.com>
2010-12-03 13:00:21 -0600

    Look for the database.yaml template in the rails cookbook

--- rails/definitions/rails_app.rb
+++ rails/definitions/rails_app.rb
@@ -20,7 +20,8 @@ define :rails_app, :action => :deploy, :user => "root", :mode => "0755" do
   end
   
   template "#{path}/shared/config/database.yaml" do
-    source "database.yml.erb"
+    source "database.yaml.erb"
+    cookbook "rails"
     owner params[:user]
     group grp
     mode params[:mode]
