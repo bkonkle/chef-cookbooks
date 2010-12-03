0092873c22775b980a846406f461034ff2dd5e15
bkonkle <brandon.konkle@gmail.com>
2010-12-03 12:52:07 -0600

    Generate the Database YAML

--- rails/definitions/rails_app.rb
+++ rails/definitions/rails_app.rb
@@ -7,12 +7,28 @@ define :rails_app, :action => :deploy, :user => "root", :mode => "0755" do
   grp = (params[:group] or params[:user])
   sites_dir = node[:sites][:dir]
   path = (params[:path] or "#{sites_dir}/#{params[:name]}")
-
-  directory path do
+  
+  # Create necessary directories all at once
+  %w{config logs pids system}.each do |dir|
+    directory "#{path}/shared/#{dir}" do
+      owner params[:user]
+      group grp
+      mode params[:mode]
+      recursive true
+      action :create
+    end
+  
+  template "#{path}/shared/config/database.yaml" do
+    source "database.yml.erb"
     owner params[:user]
     group grp
     mode params[:mode]
-    action :create
+    variables({
+      :database => params[:database][:name],
+      :username => params[:database][:username],
+      :password => params[:database][:password],
+      :host => params[:database][:host]
+    })
   end
 
   deploy_revision path do
