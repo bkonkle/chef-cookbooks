c85b65535df4848200f7a11c74125d664ec55f05
bkonkle <brandon.konkle@gmail.com>
2010-12-03 15:20:47 -0600

    Added a template definition for the nginx site conf

--- rails/definitions/rails_app.rb
+++ rails/definitions/rails_app.rb
@@ -1,46 +1,69 @@
 define :rails_app, :action => :deploy, :user => "root", :mode => "0755" do
-  include_recipe "ruby"  
+  case params[:action]
+  when :deploy, :force_deploy, :rollback
+    
+    include_recipe "ruby"  
   
-  raise "Please provide the deploy details." unless params[:deploy_settings]
-  raise "Please provide the config template to use." unless params[:template]
+    raise "Please provide the deploy details." unless params[:deploy_settings]
+    raise "Please provide the config template to use." unless params[:template]
 
-  grp = (params[:group] or params[:user])
-  sites_dir = node[:sites][:dir]
-  path = (params[:path] or "#{sites_dir}/#{params[:name]}")
+    grp = (params[:group] or params[:user])
+    sites_dir = node[:sites][:dir]
+    path = (params[:path] or "#{sites_dir}/#{params[:name]}")
   
-  # Create necessary directories all at once
-  %w{config log pids system}.each do |dir|
-    directory "#{path}/shared/#{dir}" do
+    # Create necessary directories all at once
+    %w{config log pids system}.each do |dir|
+      directory "#{path}/shared/#{dir}" do
+        owner params[:user]
+        group grp
+        mode params[:mode]
+        recursive true
+        action :create
+      end
+    end
+  
+    template "#{path}/shared/config/database.yml" do
+      source "database.yml.erb"
+      cookbook "rails"
       owner params[:user]
       group grp
       mode params[:mode]
-      recursive true
-      action :create
+      variables({
+        :database => params[:database][:name],
+        :username => params[:database][:username],
+        :password => params[:database][:password],
+        :host => params[:database][:host]
+      })
     end
-  end
-  
-  template "#{path}/shared/config/database.yml" do
-    source "database.yml.erb"
-    cookbook "rails"
-    owner params[:user]
-    group grp
-    mode params[:mode]
-    variables({
-      :database => params[:database][:name],
-      :username => params[:database][:username],
-      :password => params[:database][:password],
-      :host => params[:database][:host]
-    })
-  end
 
-  deploy_revision path do
-    user params[:user]
-    group grp
-    action params[:action]
-    migration_command "rake db:migrate"
-    environment "RAILS_ENV" => node[:rails][:environment]
-    params[:deploy_settings].each_pair do |func_name, param_value|
-      send(func_name, param_value)
+    deploy_revision path do
+      user params[:user]
+      group grp
+      action params[:action]
+      migration_command "rake db:migrate"
+      environment "RAILS_ENV" => node[:rails][:environment]
+      params[:deploy_settings].each_pair do |func_name, param_value|
+        send(func_name, param_value)
+      end
     end
-  end
+  
+    template "/etc/nginx/sites-available/#{params[:name]}" do
+      source params[:template]
+      owner "root"
+      group "root"
+      mode "0644"
+    end
+  
+    link "/etc/nginx/sites-enabled/#{params[:name]}" do
+      to "/etc/nginx/sites-available/#{params[:name]}"
+    end
+  case :enable
+    link "/etc/nginx/sites-enabled/#{params[:name]}" do
+      to "/etc/nginx/sites-available/#{params[:name]}"
+    end
+  case :disable
+    link "/etc/nginx/sites-enabled/#{params[:name]}" do
+      action :delete
+      only_if "test -f /etc/nginx/sites-enabled/#{params[:name]}"
+    end
 end
