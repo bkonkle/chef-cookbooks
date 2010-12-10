3cd907fe4d581e1730ffde65875bb5c6b0e875a4
bkonkle <brandon.konkle@gmail.com>
2010-12-10 12:44:23 -0600

    Decoupled Nginx from Rails

---
+++ nginx/definitions/nginx-site.rb
@@ -0,0 +1,32 @@
+define :nginx_site, :action => :enable do
+  case params[:action]
+  when :enable
+    
+    include_recipe "nginx::base"
+  
+    raise "Please provide the config template to use." unless params[:template]
+  
+    template "/etc/nginx/sites-available/#{params[:name]}" do
+      source params[:template]
+      owner "root"
+      group "root"
+      mode "0644"
+    end
+  
+    link "/etc/nginx/sites-enabled/#{params[:name]}" do
+      to "/etc/nginx/sites-available/#{params[:name]}"
+      notifies :restart, resources(:service => "nginx")
+    end
+  
+  when :disable
+    
+    include_recipe "nginx::base"
+    
+    link "/etc/nginx/sites-enabled/#{params[:name]}" do
+      action :delete
+      only_if "test -f /etc/nginx/sites-enabled/#{params[:name]}"
+      notifies :restart, resources(:service => "nginx")
+    end
+    
+  end
+end
--- rails/definitions/rails_app.rb
+++ rails/definitions/rails_app.rb
@@ -1,92 +1,53 @@
-define :rails_app, :action => :deploy, :user => "root", :mode => "0755" do
-  case params[:action]
-  when :deploy, :force_deploy, :rollback
-    
-    include_recipe "ruby"
-    include_recipe "nginx::base"
-  
-    raise "Please provide the deploy details." unless params[:deploy_settings]
-    raise "Please provide the config template to use." unless params[:template]
+define :rails_app, :action => :deploy, :user => "root", :mode => "0755" do  
+  include_recipe "ruby"
 
-    grp = (params[:group] or params[:user])
-    sites_dir = node[:sites][:dir]
-    path = (params[:path] or "#{sites_dir}/#{params[:name]}")
-  
-    # Create necessary directories all at once
-    %w{config log pids system backup}.each do |dir|
-      directory "#{path}/shared/#{dir}" do
-        owner params[:user]
-        group grp
-        mode params[:mode]
-        recursive true
-        action :create
-      end
-    end
-  
-    template "#{path}/shared/config/database.yml" do
-      source "database.yml.erb"
-      cookbook "rails"
+  raise "Please provide the deploy details." unless params[:deploy_settings]
+
+  grp = (params[:group] or params[:user])
+  sites_dir = node[:sites][:dir]
+  path = (params[:path] or "#{sites_dir}/#{params[:name]}")
+
+  # Create necessary directories all at once
+  %w{config log pids system backup}.each do |dir|
+    directory "#{path}/shared/#{dir}" do
       owner params[:user]
       group grp
       mode params[:mode]
-      variables({
-        :database => params[:database][:name],
-        :username => params[:database][:username],
-        :password => params[:database][:password],
-        :host => params[:database][:host]
-      })
+      recursive true
+      action :create
     end
+  end
 
-    deploy_revision path do
-      user params[:user]
-      group grp
-      action params[:action]
-      migration_command "rake db:migrate"
-      environment "RAILS_ENV" => node[:rails][:environment]
-      params[:deploy_settings].each_pair do |func_name, param_value|
-        send(func_name, param_value)
-      end
-      notifies :run, "execute[installing required gems]"
-      notifies :restart, resources(:service => "nginx")
+  template "#{path}/shared/config/database.yml" do
+    source "database.yml.erb"
+    cookbook "rails"
+    owner params[:user]
+    group grp
+    mode params[:mode]
+    variables({
+      :database => params[:database][:name],
+      :username => params[:database][:username],
+      :password => params[:database][:password],
+      :host => params[:database][:host]
+    })
+  end
+
+  deploy_revision path do
+    user params[:user]
+    group grp
+    action params[:action]
+    migration_command "rake db:migrate"
+    environment "RAILS_ENV" => node[:rails][:environment]
+    params[:deploy_settings].each_pair do |func_name, param_value|
+      send(func_name, param_value)
     end
-    
-    execute "installing required gems" do
-      command "sudo rake gems:install RAILS_ENV=#{node[:rails][:environment]}"
-      cwd "#{path}/current"
-      action :nothing
-      ignore_failure true
-    end
+    notifies :run, "execute[installing required gems]"
+  end
   
-    template "/etc/nginx/sites-available/#{params[:name]}" do
-      source params[:template]
-      owner "root"
-      group "root"
-      mode "0644"
-    end
-  
-    link "/etc/nginx/sites-enabled/#{params[:name]}" do
-      to "/etc/nginx/sites-available/#{params[:name]}"
-      notifies :restart, resources(:service => "nginx")
-    end
-  
-  when :enable
-    
-    include_recipe "nginx::base"
-    
-    link "/etc/nginx/sites-enabled/#{params[:name]}" do
-      to "/etc/nginx/sites-available/#{params[:name]}"
-      notifies :restart, resources(:service => "nginx")
-    end
-    
-  when :disable
-    
-    include_recipe "nginx::base"
-    
-    link "/etc/nginx/sites-enabled/#{params[:name]}" do
-      action :delete
-      only_if "test -f /etc/nginx/sites-enabled/#{params[:name]}"
-      notifies :restart, resources(:service => "nginx")
-    end
-    
+  execute "installing required gems" do
+    command "sudo rake gems:install RAILS_ENV=#{node[:rails][:environment]}"
+    cwd "#{path}/current"
+    action :nothing
+    ignore_failure true
   end
 end
