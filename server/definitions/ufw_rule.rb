f26f5607046705217f224b88c437408fe0b6b3ba
bkonkle <brandon.konkle@gmail.com>
2010-12-13 16:18:13 -0600

    Revised my ufw definition, because the Upstart job actually handles enable and disable

--- server/definitions/ufw_rule.rb
+++ server/definitions/ufw_rule.rb
@@ -20,46 +20,29 @@
 define :ufw_rule, :default => false, :delete => false do
   include_recipe "server::security"
   
-  case params[:action]
-  when :allow, :deny
-  
-    if params[:default]
-      execute "ufw default #{params[:action]}" do
-        user "root"
-      end
-    else
-
-      # Build the command
-      rule_cmd = "ufw "
-      rule_cmd += "delete " if params[:delete]
-      rule_cmd += "#{params[:action]} "
-      if params[:from]
-        rule_cmd += "from #{params[:from]} "
-        rule_cmd += "to #{params[:to]} port " if params[:protocol]
-      end
-      rule_cmd += "#{params[:port]}" if params[:port]
-      if params[:protocol] and not params[:from]
-        rule_cmd += "/#{params[:protocol]}"
-      end
-      
-      # Make sure ufw is enabled
-      execute "yes | ufw enable" do
-        user "root"
-        only_if 'test "`ufw status`" = "Status: inactive"'
-      end
-      
-      # Execute it
-      execute rule_cmd  do
-        user "root"
-        notifies :reload, resources(:service => "ufw"), :immediately
-      end
-    
-    end
-  
-  when :disable
-    
-    execute "ufw disable" do
+  if params[:default]
+    execute "ufw default #{params[:action]}" do
       user "root"
+    end
+  else
+
+    # Build the command
+    rule_cmd = "ufw "
+    rule_cmd += "delete " if params[:delete]
+    rule_cmd += "#{params[:action]} "
+    if params[:from]
+      rule_cmd += "from #{params[:from]} "
+      rule_cmd += "to #{params[:to]} port " if params[:protocol]
+    end
+    rule_cmd += "#{params[:port]}" if params[:port]
+    if params[:protocol] and not params[:from]
+      rule_cmd += "/#{params[:protocol]}"
+    end
+    
+    # Execute it
+    execute rule_cmd  do
+      user "root"
+      notifies :reload, resources(:service => "ufw"), :immediately
     end
   
   end
