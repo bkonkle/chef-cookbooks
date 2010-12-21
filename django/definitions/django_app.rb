7381feec8aa01ac9dfda06fd94ea698d4d514a43
bkonkle <brandon.konkle@gmail.com>
2010-12-21 15:23:46 -0600

    Updated the django_app definition

--- django/definitions/django_app.rb
+++ django/definitions/django_app.rb
@@ -1,61 +1,59 @@
-define :django_app, :action => :deploy, :user => "root", :mode => "0755" do
+define :django_app, :action => :deploy, :user => "root", :mode => "0755",
+       :packages => {} do
   include_recipe "python"
   
   raise "Please provide the deploy details." unless params[:deploy_settings]
   
-  user = (site[:user] or "root")
-  group = (site[:group] or user)
-  mode = (site[:mode] or "0775")
+  group = (params[:group] or user)
   sites_dir = node[:sites][:dir]
+  path = (params[:path] or "#{sites_dir}/#{params[:name]}")
   venvs_dir = node[:django][:virtualenvs]
-  path = (site[:path] or "#{sites_dir}/#{domain}")
-  venv = (site[:venv] or "#{venvs_dir}/#{name}")
+  venv = (params[:virtualenv] or "#{venvs_dir}/#{params[:name]}")
   server_type = node[:django][:app_server]
-  packages = (site[:packages] or {})
-  requirements = (site[:requirements] or "#{path}/code/requirements.txt")
+  requirements = (params[:requirements] or "#{path}/code/requirements.txt")
 
   directory path do
-    owner user
+    owner params[:user]
     group group
-    mode mode
+    mode params[:mode]
     action :create
   end
 
   directory "#{path}/public" do
-    owner user
+    owner params[:user]
     group group
-    mode mode
+    mode params[:mode]
     action :create
   end
 
   directory "#{path}/logs" do
-    owner user
+    owner params[:user]
     group group
-    mode mode
+    mode params[:mode]
     action :create
   end
 
   directory "#{path}/backup" do
-    owner user
+    owner params[:user]
     group group
-    mode mode
+    mode params[:mode]
     action :create
   end
   
   git "#{path}/code" do
     repository site[:repo]
     revision site[:revision]
-    user user
+    user params[:user]
     group group
     action :sync
   end
 
   virtualenv name do
     path venv
-    owner user
+    owner params[:user]
     group group
-    mode mode
-    packages packages
+    mode params[:mode]
+    packages params[:packages]
     requirements requirements
   end
 end
