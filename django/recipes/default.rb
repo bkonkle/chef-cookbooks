402eb16188c667342685c482f53cf7e62fdab353
bkonkle <brandon.konkle@gmail.com>
2010-12-21 11:55:28 -0600

    Make sure that the sites directory exists

--- django/recipes/default.rb
+++ django/recipes/default.rb
@@ -1,5 +1,11 @@
 include_recipe "python"
 
+directory node[:sites][:dir] do
+  owner node[:sites][:user]
+  group node[:sites][:group]
+  mode node[:sites][:mode]
+end
+
 directory node[:django][:virtualenvs] do
   owner node[:sites][:user]
   group node[:sites][:group]
