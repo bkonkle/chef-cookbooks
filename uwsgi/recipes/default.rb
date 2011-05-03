f783e83685c9a87e4aeab602751e839854ca7446
bkonkle <brandon@brandonkonkle.com>
2011-05-02 22:46:45 -0500

    Shifted the apt-get update to after the addition of the uwsgi ppa key

--- uwsgi/recipes/default.rb
+++ uwsgi/recipes/default.rb
@@ -6,11 +6,11 @@ template "/etc/apt/sources.list.d/uwsgi.list" do
   owner "root"
   mode "0644"
   source "uwsgi.list.erb"
-  notifies :run, resources("execute[apt-get update]"), :immediately
 end
 
 execute "Add uWSGI PPA key" do
   command "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B33D8107"
+  notifies :run, resources("execute[apt-get update]"), :immediately
 end
 
 package "uwsgi-python"
